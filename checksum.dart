9446d4c4dbcc0c6b0387e5ffb13fe92215347865
patrickraguin <patrick.raguin@gmail.com>
2022-04-28 09:30:15 +0200

    Generate versions.json

---
+++ checksum.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert'; // for the utf8.encode method
+
+void main() {
+
+
+  final Map<String, dynamic> files = browseDirectory('${Directory.current.path}/files');
+  print(jsonEncode(files));
+  print(files);
+  final File file = File('${Directory.current.path}/files/versions.json');
+  file.writeAsStringSync(jsonEncode(files));
+}
+
+Map<String, dynamic> browseDirectory(String uri) {
+  final Map<String, dynamic> files = {};
+  final List<FileSystemEntity> entries = Directory(uri).listSync();
+  for(final FileSystemEntity entity in entries) {
+    if(entity is File) {
+      files[basenameWithoutExtension(entity.path)] = getSha1(entity.path);
+    } else {
+      files[basename(entity.path)] = browseDirectory(entity.path);
+    }
+  }
+  return files;
+}
+String getSha1(String uri) => sha1.convert(File(uri).readAsBytesSync()).toString();
